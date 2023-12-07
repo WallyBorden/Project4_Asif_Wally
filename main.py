ad048d3d0778d50a70f8028bb53e27ab36b45c45
smiqbal012 <smiqbal@syr.edu>
2023-12-07 07:36:20 -0500

    some modifications, class activities etc.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Project_4: Tumor Growth Animataion
+
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,5 @@
+#Radiation_therapy
+
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
